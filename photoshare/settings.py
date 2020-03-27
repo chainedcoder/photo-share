a7cfce6eb5072b8861ac01ca2a260e11a2b935ce
Stephen Nyamweya <stephen@Stephens-MacBook-Pro.local>
2020-03-27 15:59:00 +0300

    Fix syntax

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'photoshare',
-        'USER': 'redpulse,
+        'USER': 'redpulse',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
